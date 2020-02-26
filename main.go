248b65153af9b51c4f99d82fa55191cebdcd1a84
André Sousa <andreosousa95@gmail.com>
2020-02-26 10:06:57 +0000

    Added command line flags/arguments to sort results by name or rating average

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"strconv"
 	"strings"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
+	"github.com/aosousa/movie-rating-analyzer/maths"
 	"github.com/aosousa/movie-rating-analyzer/models"
 )
 
@@ -12,12 +15,32 @@ var (
 	directors models.Directors
 )
 
+const (
+	version = "1.0.0"
+)
+
 func main() {
+	// create slice of Director structs
 	createDirectorSlice()
 
-	// loop directors and print their statistics
-	for _, director := range directors {
-		director.PrintStatistics()
+	// check if there were extra arguments passed in the command line
+	args := os.Args
+
+	if len(args) != 1 {
+		cmd := args[1]
+
+		switch cmd {
+		case "-o", "--order":
+			handleSortingSlice(os.Args)
+		case "-h", "--help":
+			printHelp()
+		case "-v", "--version":
+			printVersion()
+		default:
+			directors.PrintStatistics()
+		}
+	} else {
+		directors.PrintStatistics()
 	}
 }
 
@@ -37,6 +60,11 @@ func createDirectorSlice() {
 		director := row[1]
 		rating, _ := strconv.Atoi(strings.Replace(row[2], "/10", "", -1)) // remove the "/10" section from the ratings column
 		handleDirector(director, rating)
+	}
+
+	// iterate over the final directors slice to calculate the average
+	for i := range directors {
+		directors[i].AverageRating = maths.Average(directors[i].Ratings)
 	}
 }
 
@@ -69,4 +97,50 @@ func handleDirector(director string, rating int) {
 
 		directors = append(directors, newDirector)
 	}
+}
+
+/*Handle sorting options
+Receives:
+	* args ([]string) - Slice of arguments passed in the command line during script execution
+*/
+func handleSortingSlice(args []string) {
+	param, order := args[2], args[3]
+
+	switch param {
+	case "name":
+		if strings.ToLower(order) == "asc" {
+			directors.SortAscendingByName()
+		} else if strings.ToLower(order) == "desc" {
+			directors.SortDescendingByName()
+		} else {
+			fmt.Println("Invalid sorting option. Accepted options: asc, desc")
+		}
+	case "rating":
+		if strings.ToLower(order) == "asc" {
+			directors.SortAscendingByAverage()
+		} else if strings.ToLower(order) == "desc" {
+			directors.SortDescendingByAverage()
+		} else {
+			fmt.Println("Invalid sorting option. Accepted options: asc, desc")
+		}
+	default:
+		fmt.Println("Invalid parameter. Accepted parameters: name, rating")
+	}
+}
+
+// Prints the list of accepted commands
+func printHelp() {
+	fmt.Printf(`Movie Rating Analyzer (version %s)
+Available commands:
+* -h | --help    Prints the list of available commands
+* -v | --version Prints the version of the script
+* -o | --order [param] [order] Prints the ratings but sorted by [param] (accepted values: name, rating) in [order] (accepted values: ASC, DESC)
+
+If the script is run without additional arguments, it will print the average rating for each director
+without as it finds them in the spreadsheet.`, version)
+}
+
+// Prints the current version of the script
+func printVersion() {
+	fmt.Printf("Movie Rating Analyzer version %s\n", version)
 }
--- models/director.go
+++ models/director.go
@@ -2,23 +2,59 @@ package models
 
 import (
 	"fmt"
-
-	"github.com/aosousa/movie-rating-analyzer/maths"
+	"sort"
 )
 
 // Director struct contains information about a film director and my rating
 // associated to their movies
 type Director struct {
-	Name    string // Director's name
-	Ratings []int  // Slice of ratings associated with this director's movies
+	Name          string  // Director's name
+	Ratings       []int   // Slice of ratings associated with this director's movies
+	AverageRating float32 // Average rating for a director's movies
 }
 
 // Directors represents a slice of Director structs
 type Directors []Director
 
-// PrintStatistics prints the statistics for a given director
-func (director Director) PrintStatistics() {
-	ratingAverage := maths.Average(director.Ratings)
+// PrintStatistics prints the statistics for all directors found
+func (directors Directors) PrintStatistics() {
+	for _, director := range directors {
+		fmt.Printf("%s: %.2f\n", director.Name, director.AverageRating)
+	}
+}
 
-	fmt.Printf("%s: %.2f\n", director.Name, ratingAverage)
+// SortAscendingByName sorts the directors slice by alphabetical ascending order and print the statistics in that order
+func (directors Directors) SortAscendingByName() {
+	sort.Slice(directors, func(i, j int) bool {
+		return directors[i].Name < directors[j].Name
+	})
+
+	directors.PrintStatistics()
+}
+
+// SortDescendingByName sorts the directors slice by alphabetical descending order and print the statistics in that order
+func (directors Directors) SortDescendingByName() {
+	sort.Slice(directors, func(i, j int) bool {
+		return directors[i].Name > directors[j].Name
+	})
+
+	directors.PrintStatistics()
+}
+
+// SortAscendingByAverage sorts the directors slice by average rating in ascending order and print the statistics in that order
+func (directors Directors) SortAscendingByAverage() {
+	sort.Slice(directors, func(i, j int) bool {
+		return directors[i].AverageRating < directors[j].AverageRating
+	})
+
+	directors.PrintStatistics()
+}
+
+// SortDescendingByAverage sorts the directors slice by average rating in descending order and print the statistics in that order
+func (directors Directors) SortDescendingByAverage() {
+	sort.Slice(directors, func(i, j int) bool {
+		return directors[i].AverageRating > directors[j].AverageRating
+	})
+
+	directors.PrintStatistics()
 }
